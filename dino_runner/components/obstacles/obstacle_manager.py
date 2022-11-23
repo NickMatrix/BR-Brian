9dd772d87a3da13725015516bad693f1138981b2
NickMatrix <118643949+NickMatrix@users.noreply.github.com>
2022-11-23 17:06:39 -0300

    ATIVIDADE 2: Brian Nicolas da Silva

---
+++ dino_runner/components/obstacles/obstacle_manager.py
@@ -0,0 +1,32 @@
+import pygame
+
+from dino_runner.utils.constants import SMALL_CACTUS
+from dino_runner.utils.constants import LARGE_CACTUS
+from dino_runner.utils.constants import BIRD
+from dino_runner.components.obstacles.cactus import Cactus
+
+
+
+
+class ObstacleManager:
+    def __init__(self):
+        self.obstacles = []
+
+    def update(self, game):
+        if len(self.obstacles) == 0:
+               self.obstacles.append(Cactus(SMALL_CACTUS))
+               self.obstacles.append(Cactus(LARGE_CACTUS))
+               
+            
+
+        for obstacle in self.obstacles:
+            obstacle.update(game.game_speed, self.obstacles)
+            if game.player.dino_rect.colliderect(obstacle.rect):
+                pygame.time.delay(500)
+                game.playing = False
+                break
+
+    def draw(self, screen):
+        for obstacle in self.obstacles:
+            obstacle.draw(screen)
+                
