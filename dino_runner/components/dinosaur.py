25dbcde4841f2cdf3647bef366de5191dff03257
NickMatrix <118643949+NickMatrix@users.noreply.github.com>
2022-11-22 23:38:52 -0300

    AULA 2: Brian Nicolas da Silva

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,13 +1,14 @@
 import pygame
+from pygame.sprite import Sprite
+
 from dino_runner.utils.constants import RUNNING, JUMPING,DUCKING
 
 X_POS = 80
 Y_POS = 310  
+Y_POS_DUCK = 340
 JUMP_VEL = 8.5
-DUCK_VEL = 8.5
 
-
-class Dinosaur:
+class Dinosaur(Sprite):
     def __init__(self):
         self.image = RUNNING[0]
         self.dino_rect = self.image.get_rect()
@@ -17,7 +18,7 @@ class Dinosaur:
         self.jump_vel = JUMP_VEL
         self.dino_run = True
         self.dino_jump = False
-        self.dino_Duck = False
+        self.dino_duck = False
 
     def run(self):
         self.image = RUNNING[0] if self.step_index < 5 else RUNNING[1]
@@ -37,16 +38,13 @@ class Dinosaur:
             self.jump_vel = JUMP_VEL
 
     def duck(self):
-        self.image = DUCKING
-        if self.dino_duck:
-            self.dino_rect.y -= self.duck_vel * 4
-            self.dino_vel = 0.8
-        if self.duck_vel < -DUCK_VEL:
-            self.dino_rect.x = X_POS
-            self.dino_duck = False
-            self.jump_vel = DUCK_VEL
-            
-
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POS
+        self.dino_rect.y = Y_POS_DUCK
+        self.step_index += 1
+        self.dino_duck = False
+       
     def update(self, user_input):
         if self.dino_run:
             self.run()
@@ -54,22 +52,21 @@ class Dinosaur:
             self.jump()
         elif self.dino_duck:
             self.duck()
-        
+            
         if user_input[pygame.K_UP] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
-        elif not self.dino_jump:
+            self.dino_duck = False
+        elif user_input[pygame.K_DOWN]and not self.dino_jump:
+            self.dino_run = False
+            self.dino_jump = False
+            self.dino_duck = True
+        
+        elif not self.dino_jump and not self.dino_duck:
             self.dino_jump = False
             self.dino_run = True
-        
-        
-        if user_input[pygame.K_DOWN] and not self.dino_duck:
-            self.dino_duck = True
-            self.dino_run = False
-        elif not self.dino_duck:
             self.dino_duck = False
-            self.dino_run = True
-
+        
         if self.step_index >= 10:
             self.step_index = 0
 
